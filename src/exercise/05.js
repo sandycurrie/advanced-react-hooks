43267a2362e809ea88680aa80d0f2e6775cda238
sandy.currie <sandy.currie@open.edu.au>
2022-04-01 10:26:00 +1100

    exercise 5 - useImperativeHandle to call methods in child (ref) components from higher up in the tree

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -2,24 +2,36 @@
 // http://localhost:3000/isolated/exercise/05.js
 
 import * as React from 'react'
+import {useImperativeHandle} from 'react'
 
 // 🐨 wrap this in a React.forwardRef and accept `ref` as the second argument
-function MessagesDisplay({messages}) {
+function MessagesDisplay({messages}, ref) {
   const containerRef = React.useRef()
   React.useLayoutEffect(() => {
     scrollToBottom()
-  })
+  });
 
   // 💰 you're gonna want this as part of your imperative methods
-  // function scrollToTop() {
-  //   containerRef.current.scrollTop = 0
-  // }
+  function scrollToTop() {
+    containerRef.current.scrollTop = 0
+  }
   function scrollToBottom() {
     containerRef.current.scrollTop = containerRef.current.scrollHeight
   }
 
+  // React.useLayoutEffect(() => {
+  //   ref.current = {
+  //     scrollToTop,
+  //     scrollToBottom,
+  //   };
+  // });
+
   // 🐨 call useImperativeHandle here with your ref and a callback function
   // that returns an object with scrollToTop and scrollToBottom
+  useImperativeHandle(ref, () => ({
+    scrollToTop,
+    scrollToBottom,
+  }));
 
   return (
     <div ref={containerRef} role="log">
@@ -32,6 +44,8 @@ function MessagesDisplay({messages}) {
     </div>
   )
 }
+
+MessagesDisplay = React.forwardRef(MessagesDisplay);
 
 function App() {
   const messageDisplayRef = React.useRef()
